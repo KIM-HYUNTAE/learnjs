e3be5e205781bf429947f96ca81956ef06cd13ed
KIM-HYUNTAE <inguhyuntae@gmail.com>
2022-12-11 01:04:48 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const player = {
+    name: "ht",
+    sayHello: function (otherName) {
+        console.log("Hello " + otherName + " nice to meet yoou");
+    },
+};
+//FIXME: sdaads
+//TODO: hihi
+console.log(player.name);
+player.sayHello("lynn");
+player.sayHello("nico");
