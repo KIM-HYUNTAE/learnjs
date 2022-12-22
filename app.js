6a435d23370329c85dab31b258f02b8822cbf50e
KIM-HYUNTAE <inguhyuntae@gmail.com>
2022-12-22 19:52:03 +0900

    2022-12-22_v1

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const player = {
-  name: "ht",
-  sayHello: function (otherName) {
-    console.log("Hello " + otherName + " nice to meet yoou");
-  },
-};
-//FIXME: sdaads
-//TODO: hihi
-console.log(player.name);
-player.sayHello("lynn");
-player.sayHello("nico");
+// const calculator = {
+//   plus: function (a, b) {
+//     return (a + b);
+//   },
+//   minus: function (a, b) {
+//     return (a - b);
+//   },
+//   times: function (a, b) {
+//     return (a * b);
+//   },
+//   divide: function (a, b) {
+//     return (a / b);
+//   },
+//   power: function (a, b) {
+//     return (a ** b);
+//   },
+// };
+
+// let num = calculator.plus(1, 2);
+// console.log(num);
+// num = calculator.minus(2, 3);
+// console.log(num);
+// num = calculator.times(3, 4);
+// console.log(num);
+// num = calculator.divide(5, 6);
+// console.log(num);
+// num = calculator.power(2, 3);
+// console.log(num);
+
+
+const title = document.getElementById("title");
+title.innerText = "Hello";
